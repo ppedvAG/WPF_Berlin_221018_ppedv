35abd311b340b120d20149dd8112727ddc34e793
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-10-21 14:12:27 +0200

    MVVM

---
+++ MVVM/ViewModel/StartViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM.ViewModel
+{
+    internal class StartViewModel : INotifyPropertyChanged
+    {
+        public int AnzahlPerson { get { return Model.Person.Personenliste.Count; } }
+
+        public CustomCommand LadeDbCmd { get; set; }
+
+        public CustomCommand ÖffneDbCmd { get; set; }
+
+        public StartViewModel()
+        {
+            LadeDbCmd = new CustomCommand
+                (
+                    p =>
+                    {
+                        Model.Person.GetPersonsFromDb();
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AnzahlPerson)));
+                    },
+                    p => AnzahlPerson == 0
+                );
+
+            ÖffneDbCmd = new CustomCommand
+                (
+                    p =>
+                    {
+                        //TODO: Nächstes Fenster öffnen
+
+                        (p as Window).Close();
+                    },
+                    p => AnzahlPerson > 0
+                );
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
